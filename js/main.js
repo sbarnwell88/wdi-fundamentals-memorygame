202c8d556a564e03d4af289c12f34fc873fe24a8
Susana Barnwell <susanabarnwell@susanas-mbp.attlocal.net>
2017-06-19 14:34:41 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(2);
